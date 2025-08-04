5896f1477cf6901fac5b546213e6049d79a72701
user <leonhuber2010@gmail.com>
2025-08-04 23:34:19 +0200

    add choose page

---
+++ app/index.tsx
@@ -0,0 +1,62 @@
+import { Link } from "expo-router"
+import React from "react"
+import {SafeAreaView, Text, StyleSheet, View, TouchableOpacity } from "react-native"
+
+export default function ChooseScreen(){
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.heading}>Splan</Text>
+            <Text style={styles.text}>Choose your role</Text>
+            <View style={styles.chooseRow}>
+                <TouchableOpacity style={styles.button}>
+                    <Link href={"/student/home"} style={styles.buText}>Student</Link>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Link href={"/teacher/home"} style={styles.buText}>Teacher</Link>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 16,
+    },
+
+    heading:{
+        fontSize: 30,
+        color: "#fff",
+        marginBottom: 50,
+    },
+
+    text: {
+        fontSize: 20,
+        color: "#fff",
+    },
+
+    chooseRow: {
+        flexDirection: "row",
+    },
+
+    button: {
+        padding: 10,
+        backgroundColor: "#006400",
+        borderRadius: 15,
+        marginTop: 20,
+        marginHorizontal: 10,
+    },
+
+    buText: {
+        fontSize: 20,
+        color: "#000",
+    },
+
+})
+
+
---
+++ app/student/home.tsx
@@ -0,0 +1,37 @@
+import { Link } from "expo-router"
+import React from "react"
+import {SafeAreaView, Text, StyleSheet, View, TouchableOpacity } from "react-native"
+
+export default function ChooseScreen(){
+
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.heading}>Splan</Text>
+            <Text style={styles.text}>Your a Student</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 16,
+    },
+
+    heading:{
+        fontSize: 30,
+        color: "#fff",
+        marginBottom: 50,
+    },
+
+    text: {
+        fontSize: 20,
+        color: "#fff",
+    },
+
+
+})
